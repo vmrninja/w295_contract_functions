020d2e656b74f1372d1d9f5eb654e5a00d83f5cc
vmrninja <62035310+vmrninja@users.noreply.github.com>
2022-02-18 16:05:14 -0500

    Baseline smart contract functions
    This is a basic version of early contract functions.

---
+++ escrow_basic.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity = 0.8.7;
+
+contract Escrow {
+    struct Sellers_table {
+        uint amount;
+        uint criteria;
+        uint buyerRetract;
+        bool paid;
+    }
+
+    mapping(address => Sellers_table) public sellers;
+    address public buyer;
+
+    constructor() {
+        buyer = msg.sender;
+    }
+
+    function addSeller(address seller, uint timeToCriteria) external payable {
+        require(msg.sender == buyer, 'only buyer can add sellers');
+        require(sellers[msg.sender].amount == 0, 'seller already exist');
+        require(msg.value > 0, 'price must be above zero');
+        sellers[seller] = Sellers_table(msg.value, block.timestamp + timeToCriteria, block.timestamp + (3 * timeToCriteria), false);
+    }
+
+    function payout() external {
+        Sellers_table storage seller = sellers[msg.sender];
+        require(seller.criteria <= block.timestamp, 'criteria not met');
+        require(seller.amount > 0, 'only the proper seller can be paid');
+        require(seller.paid == false, 'seller already paid');
+        seller.paid = true;
+        payable(msg.sender).transfer(seller.amount);
+    }
+
+    function buyerRetract(address sellerRetract) external {
+        Sellers_table storage seller = sellers[sellerRetract];
+        require(seller.paid == false, 'seller already paid');
+        require(msg.sender == buyer, 'only buyer can retract');
+        require(seller.buyerRetract <= block.timestamp, 'buyer cannot retract yet');
+        seller.paid = false;
+        payable(buyer).transfer(seller.amount);
+    }
+}
